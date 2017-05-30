c6c9eaf1734b9f2be2ac51ac0c9c06a25099f21e
ademega4 <ademega4@gmail.com>
2017-05-30 15:47:17 +0100

    my first github commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const port = process.ENV.port || 7090;
+require("http").createServer((req, res)=>{
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("hello world");
+}).listen(port, function(){
+    console.log(`listening on ${this.address().host} and port ${this.address().port}`);
+})
